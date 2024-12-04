a6edad4391e4560fd22376f087c25ff3888cb773
Bikash Mohanty <95167288+freakythunder@users.noreply.github.com>
2024-12-04 16:49:24 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Auth0Provider
   domain="dev-77xfvtq4jkx78p2i.us.auth0.com"
-  clientId="H4YHXQ9vK1TlBBuUAsQbC5Fno7ZE96F1"
+  clientId="mKZJjc2lryLUOoEsqErteQXBs8t1hsSJ"
   authorizationParams={{
     redirect_uri: "https://platoeducation.onrender.com"
   }}
