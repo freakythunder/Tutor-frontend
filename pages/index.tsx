ea0be781a4ec8dd3fdc8a995e6a31ca5211f2cf7
Bikash Mohanty <vikash070310@gmail.com>
2024-12-21 15:50:54 +0530

    next.js wrap begin , stuck in npm run build

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import HomePage from '../src/components/HomePage'; // Adjust the path as necessary
+
+const IndexPage = () => {
+  return <HomePage />;
+};
+
+export default IndexPage;
---
+++ pages/main.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import  ResizableContainer from '../src/components/ResizableContainer'; // Adjust the import path as necessary
+
+const MainPage = () => {
+  return <ResizableContainer />;
+};
+
+export default MainPage;
